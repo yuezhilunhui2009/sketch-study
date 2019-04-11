117d36ab269a9a88cd753e8d7aab37c3fc66cedd
yuezhilunhui2009 <yuezhilunhui2009@gmail.com>
2019-04-11 16:25:39 +0800

    docs: 初始化仓库

---
+++ Sketch 功能以及插件开发一览.mm
@@ -0,0 +1,337 @@
+<map>
+		<node ID="root" TEXT="Sketch 功能以及插件开发一览">		<node TEXT="界面（The Interface）" ID="2c116a0192bf6f084" STYLE="bubble" POSITION="right">
+		<node TEXT="画布（Canvas）" ID="10116a051f5f4403f" STYLE="fork">
+		</node>
+		<node TEXT="检查器（Inspector）" ID="13916a051f6df2182" STYLE="fork">
+		<node TEXT="对齐选项（Alignment Options）" ID="2be16a0520aafb14f" STYLE="fork">
+		</node>
+		<node TEXT="图层属性（Layer Properties）" ID="5f16a0521ab030a3" STYLE="fork">
+		</node>
+		<node TEXT="文本字段（Text Fields）" ID="9216a0521e61400a" STYLE="fork">
+		</node>
+		<node TEXT="尺寸约束（Resizing）" ID="18416a0522f8220ed" STYLE="fork">
+		</node>
+		<node TEXT="外观与样式（Appearance and Style）" ID="33916a0523e6b914d" STYLE="fork">
+		</node>
+		<node TEXT="导出（Exporting）" ID="3ca16a0524a8cc15" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="图层列表（Layer List）" ID="39d16a052507b9143" STYLE="fork">
+		<node TEXT="多页面（Multiple Pages）" ID="20516a052554690c6" STYLE="fork">
+		</node>
+		<node TEXT="画板（Artboards）" ID="c216a0525bbbb12f" STYLE="fork">
+		</node>
+		<node TEXT="蒙版（Masks）" ID="3a316a0525f38b131" STYLE="fork">
+		</node>
+		<node TEXT="布尔操作（Boolean Operations）" ID="23716a05264954075" STYLE="fork">
+		</node>
+		<node TEXT="符号与共享样式（Symbols and Shared Styles）" ID="3c16a05269aad024" STYLE="fork">
+		</node>
+		<node TEXT="隐藏与锁定图层（Hiding and Locking Layers）" ID="26c16a0526eb5201" STYLE="fork">
+		</node>
+		<node TEXT="图层筛选（Filtering Layers）" ID="3b016a052752dc0b8" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="工具栏（Toolbar）" ID="e816a05280c30115" STYLE="fork">
+		<node TEXT="插入（Insert）" ID="13016a052839cf132" STYLE="fork">
+		</node>
+		<node TEXT="数据（Data）" ID="36616a052acf7a196" STYLE="fork">
+		</node>
+		<node TEXT="创建符号（Create Symbol）" ID="1b316a052aee10118" STYLE="fork">
+		</node>
+		<node TEXT="视图缩放（Zoom）" ID="ef16a052b197c12b" STYLE="fork">
+		</node>
+		<node TEXT="编组（Group）" ID="1c916a052e2bcb18" STYLE="fork">
+		</node>
+		<node TEXT="取消编组（Ungroup）" ID="ae16a052e928a0a9" STYLE="fork">
+		</node>
+		<node TEXT="编辑图形（Edit）" ID="19116a052ed52a148" STYLE="fork">
+		</node>
+		<node TEXT="旋转（Rotate）" ID="5016a052f992b0de" STYLE="fork">
+		</node>
+		<node TEXT="设置遮罩（Mask）" ID="1da16a052fbdc70ec" STYLE="fork">
+		</node>
+		<node TEXT="图层缩放（Scale）" ID="11416a0533f06e18f" STYLE="fork">
+		</node>
+		<node TEXT="图层扁平化（Flatten）" ID="36b16a05386b5f17b" STYLE="fork">
+		</node>
+		<node TEXT="并集（Union）" ID="30f16a054454860be" STYLE="fork">
+		</node>
+		<node TEXT="扣除（Subtract）" ID="1216a0544b5b10bc" STYLE="fork">
+		</node>
+		<node TEXT="交集（Intersect）" ID="32216a0544da7a02a" STYLE="fork">
+		</node>
+		<node TEXT="差异（Difference） " ID="3e516a0545074d0b8" STYLE="fork">
+		</node>
+		<node TEXT="视图（View）" ID="1a916a05468cf200d" STYLE="fork">
+		</node>
+		<node TEXT="预览原型（Play）" ID="19a16a0546ec5a0ec" STYLE="fork">
+		</node>
+		<node TEXT="云盘（Cloud）" ID="26216a054a880a027" STYLE="fork">
+		</node>
+		<node TEXT="导出（Export）" ID="30116a054acaed063" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		<node TEXT="图层（Layer Basics）" ID="2e16a054aebe3035" STYLE="bubble" POSITION="right">
+		<node TEXT="添加图层（Adding Layers）" ID="3c316a054c96f8188" STYLE="fork">
+		</node>
+		<node TEXT="选中图层（Selecting Layers）" ID="17516a054cd03b0b2" STYLE="fork">
+		</node>
+		<node TEXT="移动图层（Moving Layers）" ID="15c16a054d1486057" STYLE="fork">
+		</node>
+		<node TEXT="改变尺寸（Resizing Layers）" ID="36b16a054d5b5e166" STYLE="fork">
+		</node>
+		<node TEXT="尺寸约束（Resizing Constraints）" ID="a416a054e8f930c2" STYLE="fork">
+		</node>
+		<node TEXT="编辑图层（Editing Layers）" ID="f616a0551f88b03e" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="图形（Shapes）" ID="f516a055ca02807a" STYLE="bubble" POSITION="right">
+		<node TEXT="编辑图形（Editing Shapes）" ID="27116a055cc7bd194" STYLE="fork">
+		</node>
+		<node TEXT="布尔操作（Boolean Operations）" ID="4216a055e4b0c14b" STYLE="fork">
+		</node>
+		<node TEXT="旋转与变形（Rotate and Transform）" ID="2e816a055e92dc00b" STYLE="fork">
+		</node>
+		<node TEXT="蒙版（Masking）" ID="19b16a055f022d0f7" STYLE="fork">
+		</node>
+		<node TEXT="剪刀工具（Scissors）" ID="8e16a055fa8140f2" STYLE="fork">
+		</node>
+		<node TEXT="旋转复制（Rotate Copies）" ID="23716a055fe4af0af" STYLE="fork">
+		</node>
+		<node TEXT="铅笔工具（Pencil）" ID="23716a056149ee017" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="矢量编辑（Vector Editing）" ID="33716a0561a5de15" STYLE="bubble" POSITION="right">
+		<node TEXT="矢量工具（Vector Tool）" ID="15416a05620b580cd" STYLE="fork">
+		</node>
+		<node TEXT="点与路径（Points and Paths）" ID="11b16a05623a1d10a" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="文本（Text）" ID="1da16a0562a8800eb" STYLE="bubble" POSITION="right">
+		<node TEXT="文本检查器（Text Inspector）" ID="bd16a0562fbb10b4" STYLE="fork">
+		</node>
+		<node TEXT="文本样式（Text Styles）" ID="1fa16a056371840b7" STYLE="fork">
+		</node>
+		<node TEXT="文本路径（Text on Path）" ID="17116a05639614016" STYLE="fork">
+		</node>
+		<node TEXT="转换为轮廓（Convert to Outlines）" ID="28116a05640b4a0b8" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="图片（Images）" ID="1e916a05649597067" STYLE="bubble" POSITION="right">
+		<node TEXT="位图编辑（Bitmap Editing）" ID="1c016a0564ee1a066" STYLE="fork">
+		</node>
+		<node TEXT="颜色调整（Color Adjust）" ID="3a116a056526b40d2" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="数据（Data）" ID="1d016a05655658179" STYLE="bubble" POSITION="right">
+		</node>
+		<node TEXT="组织图层（Grouping Layers）" ID="2d516a0566e485129" STYLE="bubble" POSITION="right">
+		<node TEXT="图层编组（Groups）" ID="b816a0567b75e14f" STYLE="fork">
+		</node>
+		<node TEXT="画板（Artboards）" ID="3e816a05682f360c7" STYLE="fork">
+		</node>
+		<node TEXT="页面（Pages）" ID="25f16a05689332149" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="样式（Styling）" ID="3a416a0568bd1305a" STYLE="bubble" POSITION="right">
+		<node TEXT="填充（Fills）" ID="7e16a056978f305d" STYLE="fork">
+		</node>
+		<node TEXT="边框（Borders）" ID="27116a0569a96c03d" STYLE="fork">
+		</node>
+		<node TEXT="阴影（Shadows）" ID="39c16a0569c9ba04d" STYLE="fork">
+		</node>
+		<node TEXT="模糊（Blurs）" ID="34516a0569ed0f084" STYLE="fork">
+		</node>
+		<node TEXT="颜色（Colors）" ID="2db16a056a554e088" STYLE="fork">
+		</node>
+		<node TEXT="渐变（Gradients）" ID="18b16a056a9cfb037" STYLE="fork">
+		</node>
+		<node TEXT="预设（Presets）" ID="2e816a056adb84004" STYLE="fork">
+		</node>
+		<node TEXT="图层样式（Layer Styles）" ID="2c916a056b396f0c7" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="符号（Symbols）" ID="416a056b934f0c5" STYLE="bubble" POSITION="left">
+		<node TEXT="创建符号（Creating Symbols）" ID="27916a056bc589163" STYLE="fork">
+		</node>
+		<node TEXT="编辑符号（Editing Symbols）" ID="1d16a056c066701e" STYLE="fork">
+		</node>
+		<node TEXT="覆盖符号（Symbol Override）" ID="30e16a056c32b403c" STYLE="fork">
+		</node>
+		<node TEXT="组织符号（Organizing Symbols）" ID="c316a056e13aa08b" STYLE="fork">
+		</node>
+		<node TEXT="嵌套符号（Nested Symbols）" ID="23416a057008cc0f7" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="库（Libraries）" ID="37c16a057053a41" STYLE="bubble" POSITION="left">
+		<node TEXT="添加库（Adding Libraries）" ID="3ac16a0570928703d" STYLE="fork">
+		</node>
+		<node TEXT="库符号（Library Symbols）" ID="1ac16a0570f47109" STYLE="fork">
+		</node>
+		<node TEXT="库样式（Library Styles）" ID="a416a05766b4a082" STYLE="fork">
+		</node>
+		<node TEXT="更新库（Library Update）" ID="2af16a0576b3a0026" STYLE="fork">
+		</node>
+		<node TEXT="分享库（Shared Librares）" ID="2cf16a05772fde026" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="原型（Prototyping）" ID="3716a0577628901a" STYLE="bubble" POSITION="left">
+		<node TEXT="链接（Links）" ID="37316a0577b01b198" STYLE="fork">
+		</node>
+		<node TEXT="热点（Hotspots）" ID="7f16a0577cd76129" STYLE="fork">
+		</node>
+		<node TEXT="固定元素（Fixed Elements）" ID="ee16a0577f8a914d" STYLE="fork">
+		</node>
+		<node TEXT="启动入口（Start Points）" ID="32616a05787aa811a" STYLE="fork">
+		</node>
+		<node TEXT="预览（Previewing）" ID="10216a0578e0650f6" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="画布（Canvas）" ID="3df16a0579259d0a" STYLE="bubble" POSITION="left">
+		<node TEXT="像素缩放模式（Pixel Zoom）" ID="21516a057955b10be" STYLE="fork">
+		</node>
+		<node TEXT="标尺、参考线、网格（Rulers、Guides、Grids）" ID="1e416a0579c41705f" STYLE="fork">
+		</node>
+		<node TEXT="测量（Measuring）" ID="5716a0579f646152" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="导出（Exporting）" ID="db16a057ab6100ef" STYLE="bubble" POSITION="left">
+		<node TEXT="配置导出选项（Make Exportable）" ID="b716a057b1687146" STYLE="fork">
+		</node>
+		<node TEXT="切片（Slices）" ID="25416a057e514f11a" STYLE="fork">
+		</node>
+		<node TEXT="文件格式（File Formats）" ID="31616a057e706d032" STYLE="fork">
+		</node>
+		<node TEXT="导出画板（Exporting Artboards）" ID="1ec16a057eba2013f" STYLE="fork">
+		</node>
+		<node TEXT="代码导出（Code Export）" ID="17b16a057efe110ce" STYLE="fork">
+		</node>
+		<node TEXT="打印（Printing）" ID="35c16a05802938072" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="首选项（Preferences）" ID="15816a05806a21089" STYLE="bubble" POSITION="left">
+		<node TEXT="常用（General）" ID="1c216a0580d9890e4" STYLE="fork">
+		</node>
+		<node TEXT="画布（Canvas）" ID="28716a05814b020db" STYLE="fork">
+		</node>
+		<node TEXT="图层（Layers）" ID="29016a0581842315a" STYLE="fork">
+		</node>
+		<node TEXT="插件（Plugins）" ID="fa16a05819b6306a" STYLE="fork">
+		</node>
+		<node TEXT="数据（Data）" ID="2cd16a0581b465032" STYLE="fork">
+		</node>
+		<node TEXT="库（Libraries）" ID="34016a0581d16618" STYLE="fork">
+		</node>
+		<node TEXT="导出预设（Export Presets）" ID="27f16a0581eb1b0cb" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="插件（Plugins）" ID="21a16a05834c03074" STYLE="bubble" POSITION="left">
+		<node TEXT="安装插件（Installing Plugins）" ID="1aa16a05836ed00c2" STYLE="fork">
+		</node>
+		<node TEXT="关闭插件（Disabling Plugins）" ID="29d16a058394f502d" STYLE="fork">
+		</node>
+		<node TEXT="创建插件（Creating Plugins）" ID="27116a0583d0d005c" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="插件开发" ID="8616a0583fc5212b" STYLE="bubble" POSITION="left">
+		<node TEXT="插件基础" ID="ad16a0602e4dc06e" STYLE="fork">
+		<node TEXT="术语" ID="3b916a060a996a03" STYLE="fork">
+		<node TEXT="插件（Plugin）" ID="e916a060c7a1b06d" STYLE="fork">
+		</node>
+		<node TEXT="插件包（Plugin Bundle）" ID="2c816a060cb577055" STYLE="fork">
+		</node>
+		<node TEXT="动作（Action）" ID="e316a060cdb2c0d9" STYLE="fork">
+		</node>
+		<node TEXT="命令（Command）" ID="15e16a060cfc71184" STYLE="fork">
+		</node>
+		<node TEXT="处理程序（Handler）" ID="38516a060d35a90d5" STYLE="fork">
+		</node>
+		<node TEXT="脚本（Script）" ID="1ab16a060e8430086" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="如何开发一个插件" ID="29016a060ac0840a1" STYLE="fork">
+		<node TEXT="按 Control + Shift + K 打开 Run Script 面板" ID="3b116a060ed924024" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="JavaScript 小笔记" ID="11916a060af9b1107" STYLE="fork">
+		<node TEXT="不会 JavaScript 就去学习" ID="21816a062ec43604" STYLE="fork">
+		</node>
+		<node TEXT="可以使用 ES6" ID="22416a062b9367017" STYLE="fork">
+		</node>
+		<node TEXT="宿主不是 browser 或 Node 环境" ID="da16a062bf7c4051" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		<node TEXT="第一个插件" ID="3b716a06051d96088" STYLE="fork">
+		<node TEXT="准备工作" ID="30316a066b784d064" STYLE="fork">
+		</node>
+		<node TEXT="使用 skpm 创建新插件" ID="6916a066c81f2181" STYLE="fork">
+		</node>
+		<node TEXT="运行插件" ID="1f816a067d26a3121" STYLE="fork">
+		<node TEXT="build 插件" ID="6c16a067d8600113" STYLE="fork">
+		</node>
+		<node TEXT="启动 Sketch，打开一个文档" ID="1b516a067e004e09e" STYLE="fork">
+		</node>
+		<node TEXT="选择 Plugins > my-plugin > My Command" ID="19716a067e20f7004" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="插件目录结构" ID="2ec16a067fed441" STYLE="fork">
+		<node TEXT=".gitignore" ID="24e16a0681248f015" STYLE="fork">
+		</node>
+		<node TEXT="README.md" ID="3e416a0681552710c" STYLE="fork">
+		</node>
+		<node TEXT="src" ID="916a0681663f0fc" STYLE="fork">
+		<node TEXT="manifest.json" ID="13116a06818902052" STYLE="fork">
+		</node>
+		<node TEXT="my-command.js" ID="1116a0681ccc7101" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="node_modules" ID="ef16a0681e1bd149" STYLE="fork">
+		<node TEXT="skpm" ID="3216a06820739163" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="my-plugin.sketchplugin" ID="4716a0682105e154" STYLE="fork">
+		<node TEXT="Contents" ID="5716a068233a8012" STYLE="fork">
+		<node TEXT="Resources" ID="2e716a068246060d6" STYLE="fork">
+		</node>
+		<node TEXT="Sketch" ID="12c16a06826092015" STYLE="fork">
+		<node TEXT="manifest.json" ID="1616a0682742c11e" STYLE="fork">
+		</node>
+		<node TEXT="my-command.js" ID="21d16a068291c0177" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		</node>
+		<node TEXT="package.json" ID="32b16a0682a9ca0c6" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="发布插件" ID="2b616a069253c80a8" STYLE="fork">
+		<node TEXT="首次发布" ID="1fc16a069270400b7" STYLE="fork">
+		</node>
+		<node TEXT="更新" ID="7e16a069444de177" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		<node TEXT="开发环境" ID="3ad16a0605638103b" STYLE="fork">
+		</node>
+		<node TEXT="调试" ID="4416a060628eb197" STYLE="fork">
+		</node>
+		<node TEXT="Action API" ID="f716a060651bf11e" STYLE="fork">
+		</node>
+		<node TEXT="发布插件" ID="2d516a06070a4808a" STYLE="fork">
+		</node>
+		<node TEXT="插件包" ID="10016a0607284e082" STYLE="fork">
+		</node>
+		<node TEXT="插件安装位置" ID="1f716a0607840f053" STYLE="fork">
+		</node>
+		<node TEXT="CocoaScript" ID="6e16a060868da196" STYLE="fork">
+		</node>
+		<node TEXT="SketchTool" ID="2d16a0608c57d01b" STYLE="fork">
+		</node>
+		<node TEXT="第三方 APP 集成" ID="32416a06090c3f023" STYLE="fork">
+		</node>
+		</node>
+</node>
+</map>
